b2cb11340e1102007bb54a2eccbe355c697ed98c
Barrett Lewis <james.barrett.lewis@gmail.com>
2019-02-17 22:26:40 -0600

    prompt user for credentials during setup

--- setup.sh
+++ setup.sh
@@ -19,6 +19,33 @@ if [ ! -f $confdir/iblocklist.lists ]; then
 fi
 
 
-# schedule update to run daily
+credpath=$confdir/iblocklist.cred # path also hardcoded in ipset-update.sh
+skipCred=0
+if [ -f "$credpath" ]; then
+  . "$credpath"
+  if [ -n "$IBL_USER" ] && [ -n "$IBL_PIN" ]; then
+    printf "\nI-Blocklist subscription credentials exist in %s\n" "$credpath"
+    read -p "Recreate? " -n 1 -r; echo;
+    [ "$(echo "$REPLY" | tr '[:upper:]' '[:lower:]')" = "y" ] || skipCred=1
+  fi
+fi
+if [ "$skipCred" != "1" ]; then
+  printf "\nEnter I-BlockList subscription credentials if you have them.\n"
+  printf "  Leave this blank if not, but many blocklists require it.\n"
+  printf "  More info at %s\n" "$confdir/iblocklist.lists"
+  printf "  Credentials can be changed later at %s\n" "$credpath"
+  printf "user? "
+  read ibluser
+  printf "pin? "
+  read iblpin
+  printf "# credentials for iblocklist subscriptions\n" > $credpath
+  printf "# if either is not set, credentials will not be provided during updates\n" >> $credpath
+  printf "# IBL and SBL lists are paid subscriptions and require credentials\n" >> $credpath
+  printf "IBL_USER=%s\n" "$ibluser" >> $credpath
+  printf "IBL_PIN=%s\n" "$iblpin" >> $credpath
+  log "Created $credpath"
+fi
+
+echo Scheduling update to run daily
 cp ipset-update.sh /etc/cron.daily/ipset-update # no '.sh' extensions here
 
